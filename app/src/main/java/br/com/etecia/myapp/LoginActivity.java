f443ed22454665cecc3387b0413b9a04600dda66
biel5279 <gabrielaraujo0704@gmail.com>
2023-05-11 22:12:20 -0300

    buttom

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,7 +2,9 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        MenuActivity.class));
+                finish();
+            }
+        },5000);
     }
 }
